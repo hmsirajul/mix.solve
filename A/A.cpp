5606185b01e6c8d58a3713222423e82f41913330
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2024-05-10 21:16:41 +0600

    BIUPC
    University programming content Problem

---
+++ A/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+
+    vector<int> years_hosted = {2014,2022, 2023};
+
+    int num_hosted = years_hosted.size();
+
+    cout << num_hosted << endl;
+
+    return 0;
+}
