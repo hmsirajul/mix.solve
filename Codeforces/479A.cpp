b0e9926907da2664e7b3941ae30da31964f9b524
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2024-10-23 00:17:04 +0600

    479A

---
+++ Codeforces/479A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+
+    int a,b,c;
+    cin >>a >>b>>c;
+    int ans5 = a+b+c;
+    int ans1 =a+(b*c);
+    int ans2 = a*(b+c);
+    int ans3 = a*b*c;
+    int ans4 = (a+b)*c;
+
+    int ans = max (ans4,(max (ans3,(max(ans2,max(ans5,ans2))))));
+
+    cout << ans ;
+    return 0;
+}
