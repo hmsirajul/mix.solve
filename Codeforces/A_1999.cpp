68f800008e9808a7afc52dcc2482eb769ff778d7
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2025-02-12 18:21:03 +0600

    A. A+B Again?

---
+++ Codeforces/A_1999.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    
+    cout << (n/10) +(n%10) << endl;
+
+}
+int main(){
+    int t;
+    cin >> t;
+    for (int i =0;i<t;i++){
+        solve();
+    }
+
+
+    return 0;
+}
