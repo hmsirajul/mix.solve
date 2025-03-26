96eba371f958876fd8ddbca9c1f0ad6271b2451a
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2025-03-27 00:12:12 +0600

    A. Love Story

---
+++ Codeforces/A_1829.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s, c = "codeforces";
+        cin >> s;
+        int ans = 0;
+        for (int i = 0; i < 10; i++)
+        {
+
+            if (s[i] != c[i])
+            {
+                ans++;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
