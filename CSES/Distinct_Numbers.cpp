ef578e5ecff427238f0d60168b3881759930ea1b
MD.SIRAJUL ISLAM <155076937+hmsirajul@users.noreply.github.com>
2025-09-15 17:02:10 +0600

    Distinct_Numbers

---
+++ CSES/Distinct_Numbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+
+    int dc = 1; 
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[i - 1]) {
+            dc++;
+        }
+    }
+
+    cout << dc << "\n";
+    return 0;
+}
